c12ffbac3f270204a4942999973bdcd73c212426
alejandro <alegalvezlo2@gmail.com>
2025-02-15 23:01:42 +0100

    Query Selector

---
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,16 @@
+// Query Selector
+
+const heading = document.querySelector('.header__texto h2') // 0 or 1 Elements
+heading.textContent = 'Nuevo Heading';
+
+console.log(heading);
+
+
+
+// Query Selector All
+
+
+
+
+
+// getElementById
