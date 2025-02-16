8c73f33bafb18b56bf069be88f305c6f49b40ab0
alejandro <alegalvezlo2@gmail.com>
2025-02-16 20:12:58 +0100

    getElementByID & QuerySelectorAll

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -5,12 +5,19 @@ heading.textContent = 'Nuevo Heading';
 
 console.log(heading);
 
-
-
 // Query Selector All
 
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces[0]);
 
+enlaces[0].textContent = 'New Text';
 
+//enlaces[0].href = 'http://google.com';
 
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
 
 // getElementById
+
+const heading2 = document.getElementById('heading');
+Console.log(heading2);
