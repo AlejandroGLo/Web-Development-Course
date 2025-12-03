0edceef155401d015fd781182527e3f3976ab9dc
alejandro <alegalvezlo2@gmail.com>
2025-12-03 15:22:44 +0100

    Getting started again

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -15,9 +15,29 @@ enlaces[0].textContent = 'New Text';
 //enlaces[0].href = 'http://google.com';
 
 enlaces[0].classList.add('nueva-clase');
-enlaces[0].classList.remove('navegacion__enlace');
+//enlaces[0].classList.remove('navegacion__enlace');
 
 // getElementById
 
 const heading2 = document.getElementById('heading');
 Console.log(heading2);
+
+// Generar un nuevo enlance
+const nuevoEnlace = document.createElement('A'); //Java recomienda en mayusc
+
+// Agregar href
+nuevoEnlace.href = 'nuevo-enlance.html';
+
+// Agregar el texto
+nuevoEnlace.textContent = 'Online Store';
+
+// Agregar la clase
+nuevoEnlance.classList.add('navegacion__enlance')
+
+// Agregarlo al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
+
+
