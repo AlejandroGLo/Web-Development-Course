884072fbcfabe2bbb591d69ff9fdf3c489ce0cf2
alejandro <alegalvezlo2@gmail.com>
2026-02-10 19:00:54 +0100

    continue

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -1,43 +1,170 @@
-// Query Selector
+// querySelector - Returns none or up to 1 element that matches the selector
 
-const heading = document.querySelector('.header__texto h2') // 0 or 1 Elements
-heading.textContent = 'Nuevo Heading';
-
+const heading = document.querySelector(".header__texto h2"); //0 or 1 elements
+heading.textContent = "Nuevo Heading";
 console.log(heading);
 
-// Query Selector All
 
-const enlaces = document.querySelectorAll('.navegacion a');
-console.log(enlaces[0]);
+// querySelectorAll - Returns none or up to all elements that matches the selector
 
-enlaces[0].textContent = 'New Text';
-
-//enlaces[0].href = 'http://google.com';
-
+const enlaces = document.querySelectorAll(".navegacion a");
+console.log(enlaces); //(enlaces[0] -> Nos muestra el primer enlace)
+enlaces[0].textContent = "Nuevo Enlace";
 enlaces[0].classList.add('nueva-clase');
-//enlaces[0].classList.remove('navegacion__enlace');
+// enlaces[0].classList.remove('navegacion__enlace');
 
-// getElementById
+// getElementById old way
 
-const heading2 = document.getElementById('heading');
-Console.log(heading2);
+//const heading2 = document.getElementById("heading");
+//console.log(heading2);
 
-// Generar un nuevo enlance
-const nuevoEnlace = document.createElement('A'); //Java recomienda en mayusc
+// Generar un nuevo enlace
 
-// Agregar href
-nuevoEnlace.href = 'nuevo-enlance.html';
+const nuevoEnlace = document.createElement('a');
 
-// Agregar el texto
-nuevoEnlace.textContent = 'Online Store';
+//agregar href
 
-// Agregar la clase
-nuevoEnlance.classList.add('navegacion__enlance')
+nuevoEnlace.href = "nuevo-enlace.html";
 
-// Agregarlo al documento
+//agregar texto
+
+nuevoEnlace.textContent = "Tienda Online";
+
+//agregar la clase
+
+nuevoEnlace.classList.add('navegacion__enlace');
+
+//agregarlo al documento
+
 const navegacion = document.querySelector('.navegacion');
+
 navegacion.appendChild(nuevoEnlace);
 
-console.log(nuevoEnlace); // a
+console.log(nuevoEnlace);
 
 
+// Eventos
+
+console.log(1);
+
+window.addEventListener('load', function () { //load espera a que el js y los archivos que dependen del html esten listos
+
+    console.log(2);
+});
+
+
+document.addEventListener('DOMContentLoaded', function () { //solo espera a que el html este listo, pero no espera al css o imagenes
+    console.log(3);
+});
+
+console.log(5);
+
+// window.onscroll = function () {
+//     console.log("Scrolleando...");
+// }
+
+// Seleccionar elementos y asociarles un evento
+
+// const btnEnviar = document.querySelector('.boton--primario');
+// btnEnviar.addEventListener('click', function (e) {
+//     console.log(e);
+//     e.preventDefault(); //Evita que el formulario se envie
+
+//     //Validar formulario
+
+
+//     console.log('enviando formulario...');
+// })
+
+//Eventos de los inputs y textarea
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+formulario.addEventListener('submit', function (e) {
+    console.log(e);
+    e.preventDefault(); //Evita que el formulario se envie
+
+    //Validar formulario
+    const { nombre, email, mensaje } = datos;
+
+    if (nombre === '' || email === '' || mensaje === '') {
+        mostrarAlerta('Todos los campos son obligatorios', true);
+        return; //Detiene la ejecucion del codigo
+    }
+    //crear alerta de exito
+    mostrarAlerta('Mensaje enviado correctamente', false);
+
+    //Limpiar formulario
+    datos.nombre = '';
+    datos.email = '';
+    datos.mensaje = '';
+
+    formulario.reset();
+})
+
+
+function leerTexto(e) {
+    //console.log(e.target.value);
+
+    datos[e.target.id] = e.target.value;
+
+    // console.log(datos);
+}
+
+function mostrarAlerta(mensaje, error = null) {
+    const alerta = document.createElement('p');
+    alerta.textContent = mensaje;
+
+
+    if (error) {
+        alerta.classList.add('error');
+    } else {
+        alerta.classList.add('exito');
+    }
+
+    formulario.appendChild(alerta);
+
+    //Eliminar la alerta despues de 3 segundos
+    setTimeout(function () {
+        alerta.remove();
+    }, 3000);
+}
+
+// //Error en pantalla
+// function mostrarError(mensaje) {
+//     const error = document.createElement('p');
+//     error.textContent = mensaje;
+//     error.classList.add('error');
+
+//     formulario.appendChild(error);
+
+//     //Eliminar la alerta despues de 3 segundos
+//     setTimeout(function () {
+//         error.remove();
+//     }, 3000);
+// }
+
+// //Mensaje de envio
+// function mostrarExito(mensaje) {
+//     exito.classList.add('exito');
+
+//     formulario.appendChild(exito);
+
+//     //Eliminar la alerta despues de 3 segundos
+//     setTimeout(function () {
+//         exito.remove();
+//     }, 3000);
+// }
